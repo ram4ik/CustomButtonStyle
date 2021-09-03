b1827066b63b7083656776cb1af4b1dae3ef267f
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-09-03 18:09:06 +0300

    init

--- CustomButtonStyle/ContentView.swift
+++ CustomButtonStyle/ContentView.swift
@@ -7,10 +7,47 @@
 
 import SwiftUI
 
+struct PressableButtonStyle: ButtonStyle {
+    
+    let scaledAmount: CGFloat
+    
+    init(scaledAmount: CGFloat) {
+        self.scaledAmount = scaledAmount
+    }
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .scaleEffect(configuration.isPressed ? scaledAmount : 1.0)
+            .brightness(configuration.isPressed ? 0.05 : 0)
+            .opacity(configuration.isPressed ? 0.9 : 1.0)
+    }
+}
+
+extension View {
+    
+    func withPressableStyle(scaledAmount: CGFloat = 0.9) -> some View {
+        buttonStyle(PressableButtonStyle(scaledAmount: scaledAmount))
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button(action: {
+            
+        }, label: {
+            Text("Click Me!")
+                .font(.headline)
+                .foregroundColor(.white)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity)
+                .background(Color.blue)
+                .cornerRadius(10)
+                .shadow(color: Color.blue.opacity(0.6), radius: 10, x: 0.0, y: 10)
+
+        })
+        //.buttonStyle(PressableButtonStyle())
+        .withPressableStyle(scaledAmount: 0.8)
+        .padding(40)
     }
 }
 
